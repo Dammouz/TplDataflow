ad6d6e7c92be8f16155b13fd721f69310b9ba8b2
Dammouz <dammouz.dev@gmail.com>
2020-11-15 16:18:52 +0100

    Extract functions into dedicated class Part II

---
+++ TplDataflow/TplDataflow/Controllers/TplDataflowController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TplDataflow.Common;
+using TplDataflow.Dataflow;
+using TplDataflow.Model;
+
+namespace TplDataflow.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/tplDataflow")]
+    [ApiController]
+    public class TplDataflowController : ControllerBase
+    {
+        private readonly ILogger<TplDataflowController> _logger;
+        private const string WorkingDirectory = @".\imgDataflow\";
+        private const string DefaultLink = "https://raw.githubusercontent.com/Dammouz/TplDataflow/master/WikimediaPicturesOfTheDayNovemberList.txt";
+
+        public TplDataflowController(ILogger<TplDataflowController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Post method to retrieve metadata from a list of image URL.
+        /// </summary>
+        /// <param name="pathToFile">Link to text file containing the list<br />
+        /// https://raw.githubusercontent.com/Dammouz/TplDataflow/master/WikimediaPicturesOfTheDayNovemberList.txt </param>
+        /// <param name="numberOfLines">Number of maximum files to retrieve</param>
+        /// <param name="order"></param>
+        /// <returns>Some metadata of downloaded images</returns>
+        [HttpPost]
+        [Route(nameof(GetMetadataFromFile))]
+        public IEnumerable<IMetaData> GetMetadataFromFile(string pathToFile, int numberOfLines, bool order = false)
+        {
+            Console.WriteLine($"Inside {nameof(TplDataflowController)} - {nameof(GetMetadataFromFile)}");
+            CommonHelpers.CleanWorkingDirectory(WorkingDirectory);
+            var listOfMetadata = new List<IMetaData>();
+
+            if (string.IsNullOrWhiteSpace(pathToFile))
+            {
+                return Functions.ReturnOnlyOneMetadaInError("The path file is null or empty");
+            }
+
+            if (numberOfLines < 1)
+            {
+                return Functions.ReturnOnlyOneMetadaInError("You aks for number of line lower than 1");
+            }
+
+
+            // Create the members of the pipeline.
+
+            var streamTextContent = new TransformBlock<string, string>(uri => Functions.StreamTextContent(uri));
+
+            var tranformContentIntoListOfUri = new TransformBlock<string, IList<string>>(content => Functions.TranformContentIntoListOfUri(content, numberOfLines));
+
+            var transformListIntoSeveralUris = new TransformManyBlock<IList<string>, string>(listOfUri => Functions.TransformListIntoSeveralUris(listOfUri));
+
+            var downloadImageData = new TransformBlock<string, IMetaData>(url => Functions.DownloadImageData(url, WorkingDirectory));
+
+            var setStatusOfProcess = new ActionBlock<IMetaData>(metadata => Functions.SetStatusOfProcess(listOfMetadata, metadata));
+
+
+            // Connect the dataflow blocks to form a pipeline.
+
+            var linkOptions = new DataflowLinkOptions
+            {
+                PropagateCompletion = true
+            };
+
+            streamTextContent.LinkTo(tranformContentIntoListOfUri, linkOptions);
+            tranformContentIntoListOfUri.LinkTo(transformListIntoSeveralUris, linkOptions);
+            transformListIntoSeveralUris.LinkTo(downloadImageData, linkOptions);
+            downloadImageData.LinkTo(setStatusOfProcess, linkOptions);
+
+            streamTextContent.Post(pathToFile);
+
+            // Mark the head of the pipeline as complete.
+            streamTextContent.Complete();
+
+            // Wait for the last block in the pipeline to process all messages.
+            setStatusOfProcess.Completion.Wait();
+
+            return order
+                ? listOfMetadata.OrderByDescending(metadata => metadata.Status)
+                : (IEnumerable<IMetaData>)listOfMetadata;
+        }
+    }
+}
--- TplDataflow/TplDataflow/Dataflow/Functions.cs
+++ TplDataflow/TplDataflow/Dataflow/Functions.cs
@@ -1,11 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using TplDataflow.Common;
+using TplDataflow.Model;
 
 namespace TplDataflow.Dataflow
 {
-    public class Functions
+    internal static class Functions
     {
+        internal static IEnumerable<IMetaData> ReturnOnlyOneMetadaInError(string error)
+        {
+            return new List<IMetaData>
+                {
+                    new MetaData
+                    {
+                        Error = error
+                    }
+                };
+        }
+
+        internal async static Task<string> StreamTextContent(string uri)
+        {
+            Console.WriteLine($"{nameof(StreamTextContent)} - Downloading '{uri}'...");
+
+            return await new HttpClient(new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip
+            })
+            .GetStringAsync(uri);
+        }
+
+        internal static IList<string> TranformContentIntoListOfUri(string content, int numberOfLines)
+        {
+            Console.WriteLine($"{nameof(TranformContentIntoListOfUri)} - Tranforming content into list of URI...");
+
+            var listOfUri = content.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None)
+                                   .Take(numberOfLines)
+                                   .ToList();
+
+            return listOfUri;
+        }
+
+        internal static IEnumerable<string> TransformListIntoSeveralUris(IList<string> listOfUri)
+        {
+            Console.WriteLine($"{nameof(TransformListIntoSeveralUris)} - Transforming list into several URIs...");
+
+            foreach (var uri in listOfUri)
+            {
+                yield return uri;
+            }
+        }
+
+        internal static IMetaData DownloadImageData(string url, string directoryPath)
+        {
+            Console.WriteLine($"{nameof(DownloadImageData)} - Downloading image data...");
+
+            IMetaData metadata = null;
+
+            try
+            {
+                var fileName = CommonHelpers.MakeValidFileName(Path.GetFileName(url));
+                var imagePath = Path.Combine(directoryPath, fileName);
+
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(url, imagePath);
+                }
+
+                using (var image = Image.FromFile(imagePath))
+                {
+                    var width = image.Width;
+                    var height = image.Height;
+                    var creationTime = File.GetCreationTime(imagePath);
+                    var lastModificationTime = File.GetLastWriteTime(imagePath);
+
+                    metadata = new MetaData
+                    {
+                        Name = fileName,
+                        InitialUrl = url,
+                        Width = width,
+                        Height = height,
+                        CreationTime = creationTime,
+                        LastModificationTime = lastModificationTime,
+                        Folder = imagePath
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                metadata = new MetaData
+                {
+                    InitialUrl = url,
+                    Error = ex.Message
+                };
+            }
+
+            return metadata ?? new MetaData
+            {
+                Error = "Can't retrieve metadata from URL"
+            };
+        }
+
+        internal static void SetStatusOfProcess(IList<IMetaData> listOfMetadata, IMetaData metadata)
+        {
+            Console.WriteLine($"{nameof(SetStatusOfProcess)} - Seting the status of each metadata");
+            metadata.Status = string.IsNullOrEmpty(metadata.Error) ? 1 : -888;
+            listOfMetadata.Add(metadata);
+        }
     }
 }
